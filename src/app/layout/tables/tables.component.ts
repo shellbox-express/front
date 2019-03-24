ae444633a1f639a93429ef8215b347cdd75315be
AugustoBondanca <augustobondancaa@gmail.com>
2019-03-24 09:25:14 -0300

    api

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-tables',
@@ -7,8 +8,12 @@ import { routerTransition } from '../../router.animations';
     styleUrls: ['./tables.component.scss'],
     animations: [routerTransition()]
 })
-export class TablesComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+export class TablesComponent {
+    constructor(private http: HttpClient) {}
+    url = "http://gmmk7fjlvf.execute-api.us-east-1.amazonaws.com/dev";
+    update() {
+        this.http.get( this.url + '/purchases').subscribe(data => {
+            console.log('Response API Products', data);
+          });  
+    }
 }
